90449f0be870eb50a9a0628c17730ab7a064f1bc
okgo <okgo0801@gmail.com>
2015-06-09 17:14:58 +0300

    text fix

--- js/fade-plugin.js
+++ js/fade-plugin.js
@@ -16,22 +16,22 @@
                             createNavButtons: false,
                             navButtonContainer: null}, opts),
         els = (options.ignore)?$("> *:not(" + options.ignore + ")", el):$("> *", el);
-                            
+
     function setup() {
       $el.css("position", "relative");
       els.css("display", "none").css({left: 0, top: 0, position: "absolute"});
       els.filter(':first').css("display", "block");
-      
+
       if (options.createNavButtons) {
         createNavButtons();
         highlightNav();
       }
-    
+
       if (options.pauseOnMouseOver) {
         $el.mouseover(pause).mouseout(unpause);
         $('a', options.navButtonContainer || el).mouseover(pause).mouseout(unpause);
       }
-    
+
       if (options.delayStart > 0) {
         setTimeout(start, options.delayStart);
       }
@@ -39,13 +39,13 @@
         start();
       }
     }
-    
+
     function transitionTo(nextIdx) {
       $(els[current]).fadeOut(options.transitionTime);
       $(els[current = nextIdx]).fadeIn(options.transitionTime, cue);
       highlightNav();
     }
-    
+
     function manualNav(e) {
       var idx;
       this.blur();
@@ -57,7 +57,7 @@
       transitionTo(idx);
       e.preventDefault();
     }
-    
+
     function manualNav2(e) {
       var idx;
       this.blur();
@@ -69,24 +69,24 @@
       idx = $('.info_graph div').index(this);
       transitionTo(idx);
     }
-      
+
     function createNavButtons() {
       var i, nav = $('<div class="fadenav"></div>');
       for (i=0; i<els.length; i++) {
         $('<a class="nav' + i + '" href="#">&nbsp;</a>', options.navButtonContainer || el).click(manualNav).appendTo(nav);
-        $('.info_graph div').hover(manualNav2,unpause);        
+        $('.info_graph div').hover(manualNav2,unpause);
       }
-        
+
       nav.appendTo(options.navButtonContainer || el);
     }
-    
+
     function highlightNav() {
       if (options.createNavButtons) {
         $('.fadenav a', options.navButtonContainer || el).removeClass('current');
         $('.fadenav a:nth-child(' + (1 + current) + ')', options.navButtonContainer || el).addClass('current');
       }
     }
-      
+
     function start() {
       if (options.ignore) {
         $(options.ignore, el).fadeOut(options.transitionTime);
@@ -94,43 +94,43 @@
         fadeTimer = setTimeout(self.next, options.pauseTime + options.transitionTime);
       }
       else {
-        highlightNav();      
+        highlightNav();
         if (!options.manualNavigation) {
           fadeTimer = setTimeout(self.next, options.pauseTime);
         }
       }
     }
-    
+
     function pause() {
       paused = true;
       clearTimeouts();
     }
-    
+
     function unpause() {
       paused = false;
       cue();
     }
-    
+
     function clearTimeouts() {
       if (fadeTimer) {
         window.clearTimeout(fadeTimer);
         fadeTimer = null;
       }
     }
-    
+
     this.show = function(item) {
       if (typeof(els[item]) !== 'undefined') {
         clearTimeouts();
         transition(item);
       }
-      
+
       return this;
     };
-    
+
     this.currentItem = function() {
       return current;
     }
-    
+
     function cue() {
       if (paused || options.manualNavigation || (els.length < 2)) {
         return false;
@@ -138,7 +138,7 @@
       clearTimeouts();
       fadeTimer = window.setTimeout(self.next, options.pauseTime);
     }
-    
+
     this.next = function() {
       if (!options.singleLoop || ((current + 1) % els.length > 0)) {
         transitionTo((current + 1) % els.length || 0);
@@ -152,7 +152,7 @@
     $el.data('Fader', this);
     setup();
   }
-  
+
   $.fn.fadeTransition = function(options) {
     function getFader() {
       if (typeof $(this).data('Fader') === 'object') {
@@ -162,18 +162,17 @@
         return new FadeTransition(this, options);
       }
     }
-    
+
     this.fader = function() {
       if (typeof $(this).filter(':first').data('Fader') === 'object') {
         return $(this).filter(':first').data('Fader');
       }
-      
+
       return null;
     };
-    
+
     return this.each(getFader);
   };
-  
+
 
 }(jQuery));
-
